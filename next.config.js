39a8ea7d6b9768f7413858c72578f561fdf92011
Piotrek Witkowski <piwit@amazon.com>
2023-07-20 12:12:05 +0100

    Prepare for standalone build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const path = require('path')
+
+const nextConfig = {
+  compress: false,
+  output: 'standalone',
+  experimental: {
+		esmExternals: false, // optional
+		externalDir: true, // optional
+		outputFileTracingRoot: path.join(__dirname, '../../'), // monorepo option
+	}
+}
 
 module.exports = nextConfig
