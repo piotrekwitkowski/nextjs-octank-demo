6a29d1ee69d4078dbec7e1567f95cb26d9f5b21e
Piotrek Witkowski <piwit@amazon.com>
2023-07-25 16:08:35 +0100

    Add images to /assets and use local paths (no S3)

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const HOW_MANY_OFFERS = 3;
 export const OFFERS_URL = 'https://u07pi099k9.execute-api.us-east-1.amazonaws.com/offers';
-export const IMAGES_URL_PREFIX = 'https://dipzo4pfs70vh.cloudfront.net/';
+export const IMAGES_URL_PREFIX = '/assets/images/';
+// export const IMAGES_URL_PREFIX = 'https://dipzo4pfs70vh.cloudfront.net/';
