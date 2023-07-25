c207298f9373f6f9e7e2332c15ac9a205e6c8439
Piotrek Witkowski <piwit@amazon.com>
2023-07-25 15:31:52 +0100

    Fix LCP metric by increasing header font size

--- src/app/page-content-header.tsx
+++ src/app/page-content-header.tsx
@@ -2,9 +2,9 @@ export default function PageContentHeader() {
   return (
     <section className="py-4 text-center container">
       <div className="row py-lg-3">
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">Find Your Perfect Property</h1>
-          <span className="lead text-body-secondary">Discover a Diverse Range of Homes Nationwide, from Cosmopolitan City Apartments to Charming Countryside Retreats and Coastal Residences</span>
+        <div className="col-md-10 mx-auto">
+          <h1 className="fw-light display-1">Find Your Perfect Property</h1>
+          <span className="text-body-secondary display-5">Discover a Diverse Range of Homes Nationwide, from Cosmopolitan City Apartments to Charming Countryside Retreats and Coastal Residences</span>
         </div>
       </div>
     </section>
